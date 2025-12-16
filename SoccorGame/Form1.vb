e54af4b3d8d385c820f6f9fc48d4fc61f6ffe9a5
Kyosuke Miyazawa <zenmai36rai@gmail.com>
2025-12-16 15:46:06 +0900

    ビットマップ追加

--- SoccorGame/Form1.vb
+++ SoccorGame/Form1.vb
@@ -24,14 +24,16 @@
         Public height As Integer = 32
     End Class
     Private bmp As Bitmap
+    Private gnd As Bitmap
+    Private sp1 As Bitmap
     Private Team(11) As Player
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim _g As Graphics
         _g = Graphics.FromImage(bmp)
-        _g.FillRectangle(Brushes.Black, 0, 0, bmp.Width, bmp.Height)
+        _g.DrawImage(gnd, 0, 0)
         For i = 0 To 10 Step 1
             Team(i).Move()
-            _g.FillRectangle(Brushes.White, Team(i).x, Team(i).y, Team(i).width, Team(i).height)
+            _g.DrawImage(sp1, Team(i).x, Team(i).y)
         Next
         _g.Dispose()
         PictureBox1.Image = bmp
@@ -39,17 +41,19 @@
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         bmp = New Bitmap(PictureBox1.Width, PictureBox1.Height)
-        Team(0) = New Player(0, 100, 100)
-        Team(1) = New Player(1, 100, 200)
-        Team(2) = New Player(2, 100, 300)
-        Team(3) = New Player(3, 250, 100)
-        Team(4) = New Player(4, 250, 200)
-        Team(5) = New Player(5, 250, 300)
-        Team(6) = New Player(6, 400, 100)
-        Team(7) = New Player(7, 400, 200)
-        Team(8) = New Player(8, 400, 300)
-        Team(9) = New Player(9, 550, 100)
-        Team(10) = New Player(10, 550, 200)
+        gnd = New Bitmap("..\..\Ground.png")
+        sp1 = New Bitmap("..\..\SoccorPlayer1.png")
+        Team(0) = New Player(0, 150, 100)
+        Team(1) = New Player(1, 150, 200)
+        Team(2) = New Player(2, 150, 300)
+        Team(3) = New Player(3, 300, 100)
+        Team(4) = New Player(4, 300, 200)
+        Team(5) = New Player(5, 300, 300)
+        Team(6) = New Player(6, 450, 100)
+        Team(7) = New Player(7, 450, 200)
+        Team(8) = New Player(8, 450, 300)
+        Team(9) = New Player(9, 600, 100)
+        Team(10) = New Player(10, 600, 200)
         Timer1.Start()
     End Sub
 End Class
