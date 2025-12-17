5c597449d4ac9605c069b0dc9cba04693d606aa5
Kyosuke Miyazawa <zenmai36rai@gmail.com>
2025-12-17 12:03:23 +0900

    敵チーム追加

--- SoccorGame/SoccorGame/Form1.vb
+++ SoccorGame/SoccorGame/Form1.vb
@@ -59,9 +59,11 @@
     Private bmp As Bitmap
     Private gnd As Bitmap
     Private sp1 As Bitmap
+    Private sp2 As Bitmap
     Private bal As Bitmap
     Private bl As Ball
     Private Team(11) As Player
+    Private Enem(11) As Player
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim _g As Graphics
         _g = Graphics.FromImage(bmp)
@@ -76,6 +78,15 @@
             Team(i).Move(bl, Team(r))
             _g.DrawImage(sp1, Team(i).x, Team(i).y)
         Next
+        For i = 0 To 10 Step 1
+            If i = 0 Then
+                r = 10
+            Else
+                r = i - 1
+            End If
+            Enem(i).Move(bl, Enem(r))
+            _g.DrawImage(sp2, Enem(i).x, Enem(i).y)
+        Next
         bl.Move()
         _g.DrawImage(bal, bl.x, bl.y)
         _g.Dispose()
@@ -86,6 +97,7 @@
         bmp = New Bitmap(PictureBox1.Width, PictureBox1.Height)
         gnd = New Bitmap("..\..\Ground.png")
         sp1 = New Bitmap("..\..\SoccorPlayer1.png")
+        sp2 = New Bitmap("..\..\SoccorPlayer2.png")
         bal = New Bitmap("..\..\Ball.png")
         Team(0) = New Player(0, 150, 100)
         Team(1) = New Player(1, 150, 200)
@@ -98,6 +110,17 @@
         Team(8) = New Player(8, 450, 300)
         Team(9) = New Player(9, 600, 100)
         Team(10) = New Player(10, 600, 200)
+        Enem(0) = New Player(0, 550, 100)
+        Enem(1) = New Player(1, 550, 200)
+        Enem(2) = New Player(2, 550, 300)
+        Enem(3) = New Player(3, 400, 100)
+        Enem(4) = New Player(4, 400, 200)
+        Enem(5) = New Player(5, 400, 300)
+        Enem(6) = New Player(6, 250, 100)
+        Enem(7) = New Player(7, 250, 200)
+        Enem(8) = New Player(8, 250, 300)
+        Enem(9) = New Player(9, 100, 100)
+        Enem(10) = New Player(10, 600, 200)
         bl = New Ball(600, 200)
         Timer1.Start()
     End Sub
